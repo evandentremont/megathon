e2ad0fea59083b02605a688562cb8c624167b40b
Evan dEntremont <evan@evandentremont.com>
2014-11-16 12:06:32 -0400

    realtime edit passphrase

--- plugin/src/assets/js/options.js
+++ plugin/src/assets/js/options.js
@@ -6,4 +6,21 @@ $(window).load(function(){
    getPassPhrase(function(passPhrase){
      $("#private_key").val(passPhrase);
    });
+
+var publicKey;
+var publicKey;
+
+    $('#private_key').on({
+       keypress: function () {
+
+         privateKey = cryptico.generateRSAKey($('#private_key').val(), 512);
+         publicKey = cryptico.publicKeyString(privateKey);
+
+         $("#public_key").text(publicKey);
+       }
+    });
+
+
+
+
 });
